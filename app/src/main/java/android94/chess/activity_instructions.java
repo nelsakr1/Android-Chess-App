0f9fbed2837e3d55a5744a276c08df25370066d4
Melancholiax <ohnoitsaaron@gmail.com>
2017-04-29 02:56:15 -0400

    Added instructions activity, added replay activity, added up navigation to each activity.

---
+++ app/src/main/java/android94/chess/activity_instructions.java
@@ -0,0 +1,13 @@
+package android94.chess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_instructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
--- app/src/main/java/android94/chess/activity_main.java
+++ app/src/main/java/android94/chess/activity_main.java
@@ -7,8 +7,6 @@ import android.view.View;
 
 public class activity_main extends AppCompatActivity {
 
-    public static int[][] positions = new int[2][2];
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +25,12 @@ public class activity_main extends AppCompatActivity {
     }
 
     public void replay (View view) {
-        Intent intent = new Intent(this, activity_play.class);
+        Intent intent = new Intent(this, activity_replay.class);
         startActivity(intent);
     }
 
     public void instructions (View view) {
-        Intent intent = new Intent(this, activity_play.class);
+        Intent intent = new Intent(this, activity_instructions.class);
         startActivity(intent);
     }
 
